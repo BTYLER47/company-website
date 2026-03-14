11e512717edebb74bcd89bf6ad71225849df6c14
brian misiani <bmisiani47@gmail.com>
2026-03-15 02:10:18 +0300

    add printing branding url

--- backend/apps/services/urls.py
+++ backend/apps/services/urls.py
@@ -1,3 +1,8 @@
-from django.urls import path 
-from django.views.generic import TemplateView 
-urlpatterns = [path('services/', TemplateView.as_view(template_name='services.html'), name='services'), path('service-request/', TemplateView.as_view(template_name='service_request.html'), name='service_request')] 
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('services/', TemplateView.as_view(template_name='services.html'), name='services'),
+    path('service-request/', TemplateView.as_view(template_name='service_request.html'), name='service_request'),
+    path('printing-branding/', TemplateView.as_view(template_name='printing_branding.html'), name='printing_branding'),
+]
